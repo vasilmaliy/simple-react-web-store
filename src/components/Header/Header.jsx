cd199cad4fd9840686e1cb8512377b767b001a22
Maliy Vasyl <vasil.maliy14@gmail.com>
2023-04-03 21:03:35 +0300

    create top menu and sidebar

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,31 @@
+
 import styles from './Header.module.css';
 import imgLogo from './img/logo.svg';
+import imgMenu from './img/icon-menu.svg';
+import imgClose from './img/icon-close.svg';
+import imgCart from './img/icon-cart.svg';
+import imgAvatar from './img/image-avatar.png';
 import { NavLink } from 'react-router-dom';
+import { useState } from 'react';
 
 const Header = () => {
+  const [menuToggle, setMenuToggle] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuToggle(!menuToggle);
+  }
+
   return (
     <div className={styles.container}>
-      <img className={styles.logo} src={imgLogo} alt="sneakers"></img>
+      
+      <div className={styles.menu_button_container} onClick={() => toggleMenu()}> 
+          <img  className={styles.mobile_menu_button} src={menuToggle ? imgClose : imgMenu}></img> 
+      </div>
 
-      <ul className={styles.container__list}>
+      <img className={styles.logo} src={imgLogo} alt="sneakers"></img>
+      
+      <div className={styles.navbar} id={menuToggle ? styles.open : styles.close}>
+        <ul className={styles.menu_items_list}>
         <li><NavLink  
               to="/collections" 
               className={({ isActive, isPending }) =>
@@ -48,7 +66,20 @@ const Header = () => {
         >
           Contact
         </NavLink></li>
-      </ul>
+        </ul>
+      </div>
+      <label className={styles.overlay}></label>
+      
+
+      <div className={styles.right_menu_items}>
+            <div className={styles.cart_icon_container}>
+              <img className={styles.cart_icon} src={imgCart} alt="card"></img>
+            </div>
+            <div className={styles.user_icon_container}>
+              <img className={styles.user_icon} src={imgAvatar} alt="card"></img>
+            </div>
+      </div>
+
     </div>
   )
 }
